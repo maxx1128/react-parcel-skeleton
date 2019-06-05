070dfa49297938130a733dfefe69e90902b8bd4f
Max Antonucci <maxx1128@gmail.com>
2019-06-04 20:40:00 -0400

    Add emotion for styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
+/** @jsx jsx */
 import React from "react";
 import ReactDOM from "react-dom";
+import { css, jsx } from "@emotion/core";
 
 import { Router, Link } from "@reach/router";
 import Home from "./Home";
 import Static from "./Static";
 import Param from "./Param";
+import colors from "./styles/colors";
+
+const headerStyles = css`
+  padding: 1rem;
+
+  background-color: ${colors.primary.dark};
+
+  a {
+    color: ${colors.action.light};
+  }
+`;
 
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <header>
+        <header css={headerStyles}>
           <Link to="/">Homepage</Link>
         </header>
 
---
+++ src/styles/colors.js
@@ -0,0 +1,19 @@
+export default {
+  primary: {
+    dark: "#20504F",
+    base: "#4DC0B5",
+    light: "#A0F0ED"
+  },
+
+  action: {
+    dark: "#621B18",
+    base: "#E3342F",
+    light: "#F9ACAA",
+  },
+
+  mono: {
+    black: "#111",
+    gray: "#B8C2CC",
+    white: "#FFFFFF"
+  }
+};
